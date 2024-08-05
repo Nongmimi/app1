bcdf1cf786ed1decef2ae776853288d2a38d557a
worawaran jaiklang <156068688+Nongmimi@users.noreply.github.com>
2024-08-05 22:56:06 +0700

    commit component
    commit component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import './style.css'
- 
-export default function App(){
+import Calendar from './class-components';
+
+function App(){
   return(
     <>
-    <h2>DeveloperThai.com</h2>
-    <div className='title'>React & React Native</div>
+    
+    <p><center><Calendar/></center></p>
+    
+    
     </>
   )
 }
+export default App;
+ 
